b12ef7ffbc1b43856480d5a1a8755c74b63b41eb
GD-0606 <mgopi1171533@gmail.com>
2025-05-26 18:33:26 +0000

    changes in global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <>
-      <section className="w-full min-h-screen py-[100px]">section1</section>
+      <section className="w-full min-h-screen py-[100px]">
+        section1
+        <p>
+          gfyihguguggu jbjjhjhjhjkijkijkijkijkijkjkjjkjkj Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum possimus, maiores non ex
+          neque animi beatae voluptas culpa error dolorum fuga nemo vero quis consequuntur eius earum quidem aspernatur sit.
+        </p>
+        <h2 className="text-big-heading">testing</h2>
+      </section>
       <section className="w-full min-h-screen py-[100px]">section2</section>
     </>
   );
